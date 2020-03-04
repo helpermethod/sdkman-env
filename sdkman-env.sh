b762cf408683ddbe25df60d32eccc16e6b3dabfa
Oliver Weiler <oliver.weiler@meinestadt.de>
2020-03-05 00:36:07 +0100

    Add support for .sdkrc

--- sdkman-env.sh
+++ sdkman-env.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
 __sdk_env() {
-  if [[ ! -f .tool-versions ]]; then
-    __sdkman_echo_red '.tool-versions not found.'
-
-    return 1
+  if [[ -f .sdkrc ]]; then
+    return __use .sdkrc
   fi
 
+  if [[ -f .tool-versions ]]; then
+    return __use .tool-versions
+  fi
+
+  __sdkman_echo_red 'Neither .sdkrc nor .tool-versions found.'
+
+  return 1
+}
+
+__use() {
+  local file=$1
   local line
 
   while IFS= read -r line; do
-    local candidate=${line%% *}
-    local version=${line#* }
+    local candidate=${line%%[= ] *}
+    local version=${line#*[= ] }
 
     [[ ! -d "$SDKMAN_CANDIDATES_DIR/$candidate/$version" ]] && sdk install "$candidate" "$version"
     sdk use "$candidate" "$version"
-  done < .tool-versions
+  done < "$file"
 }
